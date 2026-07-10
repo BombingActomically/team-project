903d614cde4e6ecbfaf86dc074edc15dcdc62956
Darshan Patel <pateldarshan0225@gmail.com>
2026-07-10 19:31:46 +0530

    Update Sidebar.php

--- Sidebar.php
+++ Sidebar.php
@@ -1,133 +1,133 @@
 <nav class="pc-sidebar">
   <div class="navbar-wrapper">
+
+    <!-- Logo -->
     <div class="m-header flex items-center py-4 px-6 h-header-height">
       <a href="Index.php" class="b-brand flex items-center gap-3">
-        <!-- ========   Change your logo from here   ============ -->
         <img src="assets/images/logo-white.svg" class="img-fluid logo logo-lg" alt="logo" />
         <img src="assets/images/favicon.svg" class="img-fluid logo logo-sm" alt="logo" />
       </a>
     </div>
+
+    <!-- Sidebar Content -->
     <div class="navbar-content h-[calc(100vh_-_74px)] py-2.5">
       <ul class="pc-navbar">
+
+        <!-- Navigation -->
         <li class="pc-item pc-caption">
           <label>Navigation</label>
         </li>
-        <li class="pc-item">
+
         <li class="pc-item">
           <a href="Index.php" class="pc-link">
-            <span class="pc-micon">
-              <i data-feather="home"></i>
-            </span>
+            <span class="pc-micon"><i data-feather="home"></i></span>
             <span class="pc-mtext">Dashboard</span>
           </a>
         </li>
+
+        <!-- University -->
         <li class="pc-item pc-caption">
           <label>University Management</label>
           <i data-feather="feather"></i>
         </li>
+
         <li class="pc-item pc-hasmenu">
-          <a href="bc_color.php" class="pc-link">
-            <span class="pc-micon"> <i data-feather="globe"></i></span>
-            <span class="pc-mtext">All University</span>
+          <a href="javascript:void(0)" class="pc-link">
+            <span class="pc-micon"><i data-feather="globe"></i></span>
+            <span class="pc-mtext">University</span>
+            <span class="pc-arrow"><i class="ti ti-chevron-right"></i></span>
           </a>
+
+          <ul class="pc-submenu">
+            <li class="pc-item">
+              <a href="alluniversity.php" class="pc-link">All University</a>
+            </li>
+            <li class="pc-item">
+              <a href="adduniversity.php" class="pc-link">Add University</a>
+            </li>
+            <li class="pc-item">
+              <a href="universitystatus.php" class="pc-link">University Status</a>
+            </li>
+          </ul>
         </li>
-        <li class="pc-item pc-hasmenu">
-          <a href="bc_typography.php" class="pc-link">
-            <span class="pc-micon"> <i data-feather="plus-square"></i></span>
-            <span class="pc-mtext">Add University</span>
-          </a>
-        </li>
-        <li class="pc-item pc-hasmenu">
-          <a href="icon-feather.php" class="pc-link">
-            <span class="pc-micon"> <i data-feather="toggle-right"></i></span>
-            <span class="pc-mtext">University Status</span>
-          </a>
-        </li>
+
+        <!-- College -->
         <li class="pc-item pc-caption">
           <label>College Management</label>
           <i data-feather="feather"></i>
         </li>
+
         <li class="pc-item pc-hasmenu">
-          <a href="Colleges.php" class="pc-link">
-            <span class="pc-micon"> <i data-feather="home"></i></span>
-            <span class="pc-mtext">All College</span>
+          <a href="javascript:void(0)" class="pc-link">
+            <span class="pc-micon"><i data-feather="home"></i></span>
+            <span class="pc-mtext">College</span>
+            <span class="pc-arrow"><i class="ti ti-chevron-right"></i></span>
           </a>
+
+          <ul class="pc-submenu">
+            <li class="pc-item">
+              <a href="Colleges.php" class="pc-link">All College</a>
+            </li>
+            <li class="pc-item">
+              <a href="add_colleges.php" class="pc-link">Add College</a>
+            </li>
+            <li class="pc-item">
+              <a href="college_status.php" class="pc-link">College Status</a>
+            </li>
+          </ul>
         </li>
-        <li class="pc-item pc-hasmenu">
-          <a href="add_colleges.php" class="pc-link">
-            <span class="pc-micon"> <i data-feather="plus-square"></i></span>
-            <span class="pc-mtext">Add College</span>
-          </a>
-        </li>
-        <li class="pc-item pc-hasmenu">
-          <a href="college_status.php" class="pc-link">
-            <span class="pc-micon"> <i data-feather="toggle-right"></i></span>
-            <span class="pc-mtext">College Status</span>
-          </a>
-        </li>
-        <li class="pc-item pc-caption">
-          <label>Pages</label>
-          <i data-feather="monitor"></i>
-        </li>
-        <li class="pc-item pc-hasmenu">
+
+        <!-- Auth -->
+        <li class="pc-item">
           <a href="login-v1.html" class="pc-link" target="_blank">
-            <span class="pc-micon"> <i data-feather="lock"></i></span>
+            <span class="pc-micon"><i data-feather="lock"></i></span>
             <span class="pc-mtext">Login</span>
           </a>
         </li>
-        <li class="pc-item pc-hasmenu">
+
+        <li class="pc-item">
           <a href="register-v1.html" class="pc-link" target="_blank">
-            <span class="pc-micon"> <i data-feather="user-plus"></i></span>
+            <span class="pc-micon"><i data-feather="user-plus"></i></span>
             <span class="pc-mtext">Register</span>
           </a>
         </li>
+
+        <!-- Other -->
         <li class="pc-item pc-caption">
           <label>Other</label>
           <i data-feather="sidebar"></i>
         </li>
+
         <li class="pc-item pc-hasmenu">
-          <a href="#!" class="pc-link"><span class="pc-micon"> <i data-feather="align-right"></i> </span><span
-              class="pc-mtext">Menu levels</span><span class="pc-arrow"><i class="ti ti-chevron-right"></i></span></a>
+          <a href="javascript:void(0)" class="pc-link">
+            <span class="pc-micon"><i data-feather="align-right"></i></span>
+            <span class="pc-mtext">Menu Levels</span>
+            <span class="pc-arrow"><i class="ti ti-chevron-right"></i></span>
+          </a>
+
           <ul class="pc-submenu">
-            <li class="pc-item"><a class="pc-link" href="#!">Level 2.1</a></li>
+            <li class="pc-item"><a class="pc-link" href="javascript:void(0)">Level 2.1</a></li>
+
             <li class="pc-item pc-hasmenu">
-              <a href="#!" class="pc-link">Level 2.2<span class="pc-arrow"><i class="ti ti-chevron-right"></i></span></a>
+              <a href="javascript:void(0)" class="pc-link">
+                Level 2.2 <span class="pc-arrow"><i class="ti ti-chevron-right"></i></span>
+              </a>
               <ul class="pc-submenu">
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
-                <li class="pc-item pc-hasmenu">
-                  <a href="#!" class="pc-link">Level 3.3<span class="pc-arrow"><i class="ti ti-chevron-right"></i></span></a>
-                  <ul class="pc-submenu">
-                    <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
-                    <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li class="pc-item pc-hasmenu">
-              <a href="#!" class="pc-link">Level 2.3<span class="pc-arrow"><i class="ti ti-chevron-right"></i></span></a>
-              <ul class="pc-submenu">
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
-                <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
-                <li class="pc-item pc-hasmenu">
-                  <a href="#!" class="pc-link">Level 3.3<span class="pc-arrow"><i class="ti ti-chevron-right"></i></span></a>
-                  <ul class="pc-submenu">
-                    <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
-                    <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
-                  </ul>
-                </li>
+                <li class="pc-item"><a class="pc-link" href="javascript:void(0)">Level 3.1</a></li>
+                <li class="pc-item"><a class="pc-link" href="javascript:void(0)">Level 3.2</a></li>
               </ul>
             </li>
           </ul>
         </li>
+
+        <!-- Sample -->
         <li class="pc-item">
           <a href="../other/sample-page.html" class="pc-link">
-            <span class="pc-micon">
-              <i data-feather="sidebar"></i>
-            </span>
-            <span class="pc-mtext">Sample page</span>
+            <span class="pc-micon"><i data-feather="sidebar"></i></span>
+            <span class="pc-mtext">Sample Page</span>
           </a>
         </li>
+
       </ul>
     </div>
   </div>
