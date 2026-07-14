a72177ee60a172068a61cfcb2ffc5a5b48c13853
krishptl-11 <krish.ptl.2k26@gmail.com>
2026-07-14 23:07:33 +0530

    Update Sidebar.php

--- Sidebar.php
+++ Sidebar.php
@@ -36,14 +36,28 @@
 
           <ul class="pc-submenu">
             <li class="pc-item">
-              <a href="alluniversity.php" class="pc-link">All University</a>
+              <a href="alluniversity.php" class="pc-link">
+                  <span class="pc-micon">
+                    <i class="ti ti-building"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      All University
+                  </span>
+              </a>
             </li>
             <li class="pc-item">
-              <a href="adduniversity.php" class="pc-link">Add University</a>
+              <a href="adduniversity.php" class="pc-link">
+                  <span class="pc-micon">
+                    <i class="ti ti-plus"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Add University
+                  </span>
+              </a>
             </li>
           </ul>
         </li>
-
+        
         <!-- College -->
 
         <li class="pc-item pc-hasmenu">
@@ -55,10 +69,21 @@
 
           <ul class="pc-submenu">
             <li class="pc-item">
-              <a href="Colleges.php" class="pc-link">All College</a>
+              <a href="all_colleges.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-school"></i></span>
+                  <span class="pc-mtext">All College</span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="add_colleges.php" class="pc-link">Add College</a>
+              <a href="add_college.php" class="pc-link">
+                  <span class="pc-micon">
+                    <i class="ti ti-plus"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Add College
+                  </span>
+              </a>
             </li>
           </ul>
         </li>
@@ -78,38 +103,44 @@
           <ul class="pc-submenu">
 
             <li class="pc-item">
-              <a href="AllStudents.php" class="pc-link">
-                All Students
+              <a href="all_students.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-users"></i></span>
+                  <span class="pc-mtext">All Students</span>
               </a>
             </li>
 
             <li class="pc-item">
-              <a href="AddStudent.php" class="pc-link">
-                Add Student
+              <a href="add_student.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-user-plus"></i></span>
+                  <span class="pc-mtext">Add Student</span>
               </a>
             </li>
 
             <li class="pc-item">
-              <a href="Pending.php" class="pc-link">
-                Pending Verification
+              <a href="pending_students.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-clock"></i></span>
+                  <span class="pc-mtext">Pending Verification</span>
               </a>
             </li>
 
             <li class="pc-item">
-              <a href="VerifiedStudents.php" class="pc-link">
-                Verified Students
+              <a href="verified_students.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-user-check"></i></span>
+                  <span class="pc-mtext">Verified Students</span>
               </a>
             </li>
 
             <li class="pc-item">
-              <a href="RejectedStudents.php" class="pc-link">
-                Rejected Students
+              <a href="rejected_students.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-user-x"></i></span>
+                  <span class="pc-mtext">Rejected Students</span>
               </a>
             </li>
 
             <li class="pc-item">
-              <a href="BlockedStudents.php" class="pc-link">
-                Blocked Students
+              <a href="blocked_students.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-user-off"></i></span>
+                  <span class="pc-mtext">Blocked Students</span>
               </a>
             </li>
 
@@ -126,15 +157,40 @@
           </a>
 
           <ul class="pc-submenu">
+
             <li class="pc-item">
-              <a href="event_cat.php" class="pc-link">Event Categories</a>
+                <a href="all_categories.php" class="pc-link">
+                    <span class="pc-micon">
+                        <i class="ti ti-grid-dots"></i>
+                    </span>
+                    <span class="pc-mtext">
+                        Event Categories
+                    </span>
+                </a>
             </li>
+
             <li class="pc-item">
-              <a href="add_category.php" class="pc-link">Add Category</a>
+                <a href="add_category.php" class="pc-link">
+                    <span class="pc-micon">
+                        <i class="ti ti-circle-plus"></i>
+                    </span>
+                    <span class="pc-mtext">
+                        Add Category
+                    </span>
+                </a>
             </li>
+
             <li class="pc-item">
-              <a href="category_status.php" class="pc-link">Active / Inactive Categories</a>
+                <a href="category_status.php" class="pc-link">
+                    <span class="pc-micon">
+                        <i class="ti ti-toggle-right"></i>
+                    </span>
+                    <span class="pc-mtext">
+                        Active / Inactive Categories
+                    </span>
+                </a>
             </li>
+
           </ul>
         </li>
         <!-- Auth -->
@@ -151,19 +207,46 @@
 
           <ul class="pc-submenu">
             <li class="pc-item">
-              <a href="all_events.php" class="pc-link">All Events</a>
+              <a href="all_events.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-calendar-event"></i></span>
+                  <span class="pc-mtext">All Events</span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="pending_events.php" class="pc-link">Pending Events</a>
+                <a href="pending_events.php" class="pc-link">
+                    <span class="pc-micon"><i class="ti ti-calendar-time"></i></span>
+                    <span class="pc-mtext">Pending Events</span>
+                </a>
             </li>
+
             <li class="pc-item">
-              <a href="published_events.php" class="pc-link">Published Events</a>
+              <a href="published_events.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-check"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Published Events
+                  </span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="completed_events.php" class="pc-link">Completed Events</a>
+                <a href="completed_events.php" class="pc-link">
+                    <span class="pc-micon"><i class="ti ti-calendar-stats"></i></span>
+                    <span class="pc-mtext">Completed Events</span>
+                </a>
             </li>
+
             <li class="pc-item">
-              <a href="cancelled_events.php" class="pc-link">Cancelled Events</a>
+              <a href="cancelled_events.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-x"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Cancelled Events
+                  </span>
+              </a>
             </li>
           </ul>
         </li>
@@ -179,20 +262,48 @@
 
           <ul class="pc-submenu">
             <li class="pc-item">
-              <a href="allregistrations.php" class="pc-link">All Registrations</a>
+              <a href="all_registrations.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-clipboard-list"></i></span>
+                  <span class="pc-mtext">All Registrations</span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="soloregistrations.php" class="pc-link">Solo Registrations</a>
+                <a href="solo_registrations.php" class="pc-link">
+                    <span class="pc-micon"><i class="ti ti-user"></i></span>
+                    <span class="pc-mtext">Solo Registrations</span>
+                </a>
             </li>
+
             <li class="pc-item">
-              <a href="teamregistrations.php" class="pc-link">Team Registrations</a>
+              <a href="team_registrations.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-users"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Team Registrations
+                  </span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="pendingregistrations.php" class="pc-link">Pending Approvals</a>
+              <a href="pending_approvals.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-clock"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Pending Approvals
+                  </span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="cancelledregistrations.php" class="pc-link">Cancelled Registration</a>
+                <a href="cancelled_registrations.php" class="pc-link">
+                    <span class="pc-micon"><i class="ti ti-ban"></i></span>
+                    <span class="pc-mtext">Cancelled Registration</span>
+                </a>
             </li>
+
           </ul>
         </li>
     
@@ -209,13 +320,36 @@
 
           <ul class="pc-submenu">
             <li class="pc-item">
-              <a href="allteams.php" class="pc-link">All Teams</a>
+              <a href="all_teams.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-users"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      All Teams
+                  </span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="teammembers.php" class="pc-link">Team Members</a>
+              <a href="team_members.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-user"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Team Members
+                  </span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="teamdetails.php" class="pc-link">Team Details</a>
+              <a href="team_details.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-list"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Team Details
+                  </span>
+              </a>
             </li>
           </ul>
         </li>
@@ -233,10 +367,17 @@
 
           <ul class="pc-submenu">
             <li class="pc-item">
-              <a href="send_notifications.php" class="pc-link">Send Notifications</a>
+              <a href="send_notification.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-send"></i></span>
+                  <span class="pc-mtext">Send Notifications</span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="notification_history.php" class="pc-link">Notification History</a>
+                <a href="notification_history.php" class="pc-link">
+                    <span class="pc-micon"><i class="ti ti-history"></i></span>
+                    <span class="pc-mtext">Notification History</span>
+                </a>
             </li>
           </ul>
         </li>
@@ -252,15 +393,36 @@
           </a>
 
           <ul class="pc-submenu">
+                        
             <li class="pc-item">
-              <a href="profile.php" class="pc-link">Profile</a>
+              <a href="profile.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-user"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Profile
+                  </span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="change_password.php" class="pc-link">Change Password</a>
+              <a href="change_password.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-lock"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Change Password
+                  </span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="system_settings.php" class="pc-link">System Settings</a>
+                <a href="system_settings.php" class="pc-link">
+                    <span class="pc-micon"><i class="ti ti-settings"></i></span>
+                    <span class="pc-mtext">System Settings</span>
+                </a>
             </li>
+
           </ul>
         </li>
 
@@ -277,18 +439,43 @@
           </a>
 
           <ul class="pc-submenu">
+            
             <li class="pc-item">
-              <a href="generated_passes.php" class="pc-link">Generated Passes</a>
+                <a href="generated_passes.php" class="pc-link">
+                    <span class="pc-micon"><i class="ti ti-ticket"></i></span>
+                    <span class="pc-mtext">Generated Passes</span>
+                </a>
             </li>
+
             <li class="pc-item">
-              <a href="used_passes.php" class="pc-link">Used Passes</a>
+              <a href="used_passes.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-check"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Used Passes
+                  </span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="unused_passes.php" class="pc-link">Unused Passes</a>
+              <a href="used_passes.php" class="pc-link">
+                  <span class="pc-micon">
+                      <i class="ti ti-check"></i>
+                  </span>
+                  <span class="pc-mtext">
+                      Used Passes
+                  </span>
+              </a>
             </li>
+
             <li class="pc-item">
-              <a href="pass_verification.php" class="pc-link">Pass Verification</a>
+                <a href="pass_verification.php" class="pc-link">
+                    <span class="pc-micon"><i class="ti ti-scan"></i></span>
+                    <span class="pc-mtext">Pass Verification</span>
+                </a>
             </li>
+
           </ul>          
         </li>
 
@@ -304,23 +491,43 @@
 
           <ul class="pc-submenu">
             <li class="pc-item">
-              <a href="payment_overview.php" class="pc-link">Payment Overview</a>
+              <a href="payment_overview.php" class="pc-link">
+                  <span class="pc-micon"><i class="ti ti-wallet"></i></span>
+                  <span class="pc-mtext">Payment Overview</span>
+              </a>
             </li>
-            <li class="pc-item">
-              <a href="successful_payments.php" class="pc-link">Successful Payments</a>
-            </li>
-            <li class="pc-item">
-              <a href="pending_payments.php" class="pc-link">Pending Payments</a>
-            </li>
-            <li class="pc-item">
-              <a href="failed_payments.php" class="pc-link">Failed Payments</a>
-            </li>
-            <li class="pc-item">
-              <a href="transaction_reports.php" class="pc-link">Transaction Reports</a>
-            </li>
-          </ul>          
+
+        <li class="pc-item">
+            <a href="successful_payments.php" class="pc-link">
+                <span class="pc-micon"><i class="ti ti-circle-check"></i></span>
+                <span class="pc-mtext">Successful Payments</span>
+            </a>
         </li>
 
+        <li class="pc-item">
+          <a href="pending_payments.php" class="pc-link">
+              <span class="pc-micon">
+                  <i class="ti ti-clock"></i>
+              </span>
+              <span class="pc-mtext">
+                  Pending Payments
+              </span>
+          </a>
+        </li>
+
+        <li class="pc-item">
+            <a href="failed_payments.php" class="pc-link">
+                <span class="pc-micon"><i class="ti ti-circle-x"></i></span>
+                <span class="pc-mtext">Failed Payments</span>
+            </a>
+        </li>
+
+        <li class="pc-item">
+            <a href="transaction_reports.php" class="pc-link">
+                <span class="pc-micon"><i class="ti ti-report-money"></i></span>
+                <span class="pc-mtext">Transaction Reports</span>
+            </a>
+        </li>
       </ul>
     </div>
   </div>
