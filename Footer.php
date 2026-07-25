79d3d3fb8cc1da5a479481f8d465474b1dd92696
Darshan Patel <pateldarshan0225@gmail.com>
2026-07-25 21:44:29 +0530

    Update Footer.php

--- Footer.php
+++ Footer.php
@@ -3,7 +3,12 @@
     <div class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
       <!-- Left -->
       <div class="mb-2 mb-md-0 text-center text-muted">
-        © 2026 <strong>University Admin Panel</strong>. All Rights Reserved.
+        © 2026 <strong>Evenza</strong>. All Rights Reserved.
+      </div>
+
+      <!-- Right -->
+      <div class="text-center text-muted">
+        Made with <span style="color:#e25555;">♥</span> by <strong>Evenza Team</strong>
       </div>
     </div>
   </div>
